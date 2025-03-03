a44c33b55155e4dd39092b7c43426fe6b2241072
Barbara Gergely <gerbara15@gmail.com>
2025-03-03 23:05:34 +0100

    hw

---
+++ Szamitogep/Program.cs
@@ -0,0 +1,23 @@
+﻿using Szamitogepek;
+
+Szamitogep gep1 = new Szamitogep();
+Szamitogep gep2 = new Szamitogep(2048,true);
+
+Console.WriteLine("Az első gép: " + gep1);
+Console.WriteLine("A második gép: " + gep2);
+
+Szamitogep gep3 = new Szamitogep(512,false);
+Console.WriteLine("A harmadik gép: " + gep3);
+Console.WriteLine();
+
+//gep3.Kapcsol();
+//Console.WriteLine("A harmadik gép: " + gep3);
+//Console.WriteLine();
+
+
+gep1.Kapcsol();
+gep1.ProgramMasol(800);
+Console.WriteLine("Az első gép: " + gep1);
+
+gep1.ProgramMasol(400);
+Console.WriteLine("Az első gép: " + gep1);
---
+++ Szamitogep/Szamitogep.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamitogepek
+{
+    internal class Szamitogep
+    {
+        public double SzabadMemoria {  get; set; }
+        public bool Bekapcsolva {  get; set; }
+
+        public Szamitogep(double szabadMemoria, bool bekapcsolva)
+        {
+            SzabadMemoria = szabadMemoria;
+            Bekapcsolva = bekapcsolva;
+        }
+
+        public Szamitogep() //mivel megadjuk az alap paramétereket ez üresen marad
+        {
+            SzabadMemoria = 1024;
+            Bekapcsolva = false;
+        }
+
+        public void Kapcsol()
+        {
+            Bekapcsolva = !Bekapcsolva;
+            //if (Bekapcsolva) Bekapcsolva = false; else Bekapcsolva = true; ugyan az csak a másik rövidebb
+        }
+
+       public bool ProgramMasol(double programMeret)
+       {
+            if (Bekapcsolva && SzabadMemoria >= programMeret)
+    {
+                SzabadMemoria -= programMeret; // SzabadMemoria = SzabadMemoria - programMeret;
+                return true; // Másolás sikeres
+            }
+            return false; // Másolás sikertelen
+        }
+
+        public override string? ToString()
+        {
+            return $"Ez a gép {SzabadMemoria} MB szabad memóriával rendelkezik és "+(Bekapcsolva ? "be van kapcsolva!" : "ki van kapcsolva!");
+        }
+    }
+
+
+}
+//azt szeretném hogy ha nincs elég szabad memória vagy a gép ki van kapcsolva akkor írja ki hogy a másolás nem lehetsége mert a gép ki van kapcsolva vagy elfogyott a memória
